759142fd3a7abdb0df8a13332dd34850c5521058
Vivek Sharma <115217567+VivekSharma1103@users.noreply.github.com>
2025-02-24 22:40:55 +0530

    Create 24FEB2024_GFG_Stock span problem.cpp

---
+++ 24FEB2024_GFG_Stock span problem.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    vector<int> calculateSpan(vector<int>& arr) {
+        // write code here
+        int n = arr.size();
+        vector<int> ans(n,1);
+        stack<int> st;
+        
+        for(int i=0;i<n;i++)
+        {    
+            while(!st.empty() && arr[st.top()] <= arr[i]){
+                st.pop();
+            }
+            
+            if (st.empty()) {
+             ans[i] = i + 1;  
+             } 
+            else {
+                   ans[i] = i - st.top();  
+                  }
+            
+            st.push(i);
+            
+         }
+        
+        return ans;
+    }
+    
+};
