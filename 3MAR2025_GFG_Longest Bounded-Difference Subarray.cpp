8ba6899a14525e7a304441140daf8843e40b428a
Vivek Sharma <115217567+VivekSharma1103@users.noreply.github.com>
2025-03-03 20:55:25 +0530

    Create 3MAR2025_GFG_Longest Bounded-Difference Subarray.cpp

---
+++ 3MAR2025_GFG_Longest Bounded-Difference Subarray.cpp
@@ -0,0 +1,33 @@
+
+class Solution {
+  public:
+    vector<int> longestSubarray(vector<int>& arr, int x)
+    {
+        multiset<int> st;
+        int n = arr.size();
+        int l=0,r=0;
+        int ansl=-1,ansr=-1;
+        int maxlen = 0;
+        while(l<n && r<n)
+        {
+            st.insert(arr[r]);
+            while(!st.empty() && abs((*st.begin()) - *std::prev(st.end()))>x)
+            {
+                st.erase(st.find(arr[l]));
+                l++;
+            }
+
+            if(st.size()>maxlen)
+            {
+                maxlen = st.size();
+                ansl = l;
+                ansr = r;
+            }
+            r++;
+        }
+        vector<int> ans;
+        for(int i=ansl;i<=ansr;i++)
+            ans.push_back(arr[i]);
+        return ans;
+    }
+};
