eb14905781bf54fd82e12e5c57f34eaa211f7e6e
Vivek Sharma <115217567+VivekSharma1103@users.noreply.github.com>
2025-03-06 23:34:30 +0530

    Create 6MAR2025_GFG_Longest Common Subsequence.cpp

---
+++ 6MAR2025_GFG_Longest Common Subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int lcs(string &s1, string &s2) {
+        int m = s1.length();
+        int n = s2.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));  // Initialize with 0
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else if (s1[i - 1] == s2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[m][n]; // Fix here
+    }
+};
