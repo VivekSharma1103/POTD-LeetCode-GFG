380ef9ef2a4dd25e470256ba36db5a4525cf86c3
Vivek Sharma <115217567+VivekSharma1103@users.noreply.github.com>
2025-04-05 10:44:12 +0530

    Create 5.LC_Sum of ALL Subsets XOR Totals.cpp

---
+++ April2025/5.LC_Sum of ALL Subsets XOR Totals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:
+    int dfs(vector<int>& nums, int index, int currXOR) {
+        if (index == nums.size()) {
+            return currXOR;
+        }
+        // Include current element
+        int take = dfs(nums, index + 1, currXOR ^ nums[index]);
+        // Exclude current element
+        int notTake = dfs(nums, index + 1, currXOR);
+        return take + notTake;
+    }
+
+public:
+    int subsetXORSum(vector<int>& nums) {
+        return dfs(nums, 0, 0);
+    }
+};
