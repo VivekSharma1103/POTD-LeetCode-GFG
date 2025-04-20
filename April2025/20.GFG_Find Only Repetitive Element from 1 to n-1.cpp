92bf6a15415b8aea04679dda1e09c3411097924e
Vivek Sharma <115217567+VivekSharma1103@users.noreply.github.com>
2025-04-20 11:12:43 +0530

    Create 20.GFG_Find Only Repetitive Element from 1 to n-1.cpp

---
+++ April2025/20.GFG_Find Only Repetitive Element from 1 to n-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int findDuplicate(vector<int>& arr) {
+        // code here
+        long long n = arr.size();
+        
+        long long sum = accumulate(arr.begin(),arr.end(),0);
+        
+        int num = sum - ((n-1)*n /2 );
+        return num;
+    }
+};
